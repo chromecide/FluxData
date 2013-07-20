61e6207a6c0408a3b37d1d48260dea17b4a04f4b
Justin Pradier <chromecide@chromecide.com>
2013-07-20 19:14:57 +1000

    loads of updates

--- index.js
+++ index.js
@@ -1,11 +1,26 @@
-var model = require(__dirname+'/lib/model.js').Model;
-var attribute = require(__dirname+'/lib/attribute.js').Attribute;
-var entity = require(__dirname+'/lib/entity.js').Entity;
-var channel = require(__dirname+'/lib/channel.js').Channel;
-
-exports = module.exports = {
-	Model: model,
-	Attribute: attribute,
-	Entity: entity,
-	Channel: channel
-}
+;!function(exports, undefined) {
+	
+	if (typeof define === 'function' && define.amd) {	
+		define(['/lib/FluxData/lib/model.js', '/lib/FluxData/lib/attribute.js','/lib/FluxData/lib/entity.js', '/lib/FluxData/lib/channel.js'], function(model, attribute, entity, channel){
+			var exportObj = {
+				Model: model,
+				Attribute: attribute,
+				Entity: entity,
+				Channel: channel
+			}
+			return exportObj;
+		});
+	} else {
+		var model = require(__dirname+'/lib/model.js').Model;
+		var attribute = require(__dirname+'/lib/attribute.js').Attribute;
+		var entity = require(__dirname+'/lib/entity.js').Entity;
+		var channel = require(__dirname+'/lib/channel.js').Channel;
+		
+		exports = module.exports = {
+			Model: model,
+			Attribute: attribute,
+			Entity: entity,
+			Channel: channel
+		}
+	}
+}(typeof process !== 'undefined' && typeof process.title !== 'undefined' && typeof exports !== 'undefined' ? exports : window);
